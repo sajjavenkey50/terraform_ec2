597d62f8061397d137ce8950a1961fefc828f2b7
sajjavenkey50 <sajjavenkey50@gmail.com>
2021-03-25 22:27:03 +0530

    adding

--- main.tf
+++ main.tf
@@ -1,26 +1,17 @@
 variable "type" {
   type = string
 }
-
 //creating the windows system
 resource "aws_instance" "windows" {
-<<<<<<< HEAD
         ami = "ami-04f33832b669e4355"
-=======
-        ami = "ami-0afcbc82a6a511e53"
->>>>>>> 4321777b3c89f7127a15b5244c40b49181e3f8bd
         instance_type = "t2.micro"
         tags =  {
-          Name = random_pet.name.id
+          Name = "Ec2_Instance_Terraform"
         }
 }
 
 // create a security group for rdp access to the windows systems
 resource "aws_security_group" "windows-sg" {
-<<<<<<< HEAD
-=======
-name        = "${random_pet.name.id}-sg"
->>>>>>> 4321777b3c89f7127a15b5244c40b49181e3f8bd
 description = "Allow RDP inbound traffic"
  
   ingress {
@@ -39,11 +30,9 @@ description = "Allow RDP inbound traffic"
   }
 }
 
-<<<<<<< HEAD
 //Security group attachment
 resource "aws_network_interface_sg_attachment" "sg_attachment" {
   security_group_id    = aws_security_group.windows-sg.id
   network_interface_id = aws_instance.windows.primary_network_interface_id
 }
-=======
->>>>>>> 4321777b3c89f7127a15b5244c40b49181e3f8bd
+
